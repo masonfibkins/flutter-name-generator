6cc4dd569589bd1a8916905bbaad622b961924b4
Mason Fibkins <mason.fibkins@gmail.com>
2022-04-13 19:42:51 -0600

    added theme colors

--- name_gen/lib/main.dart
+++ name_gen/lib/main.dart
@@ -10,10 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Random Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.deepPurple,
+        ),
+      ),
+      home: const RandomWords(),
     );
   }
 }
