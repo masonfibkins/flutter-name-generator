500396f71857d77bf268c9b72582e7f8755a0835
Mason Fibkins <mason.fibkins@gmail.com>
2022-04-13 19:37:35 -0600

    added function to make a new route and take you to another page to view saved names

--- name_gen/lib/main.dart
+++ name_gen/lib/main.dart
@@ -27,6 +27,13 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Random Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+          ],
         ),
         body: ListView.builder(
             padding: const EdgeInsets.all(16.0),
@@ -61,6 +68,38 @@ class _RandomWordsState extends State<RandomWords> {
                 },
               );
             }));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
 
